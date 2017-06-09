61c113a2c031a198bab4a8777336d32ce526f39d
Jason Brown <browniefed@gmail.com>
2017-06-09 16:13:33 -0700

    Add basic list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import { List, AutoSizer } from "react-virtualized";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <AutoSizer>
+        {({ width, height }) => {
+          return (
+            <div style={{ width: `${width}px`, height: `${height}px` }}>
+              {width}px x {height}px
+            </div>
+          );
+        }}
+      </AutoSizer>
     );
   }
 }
