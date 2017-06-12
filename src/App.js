e020cafe236575da60ab06e08c64a88122b3aaea
Jason Brown <browniefed@gmail.com>
2017-06-12 12:11:08 -0700

    Add code

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React, { Component } from "react";
 import { List, AutoSizer } from "react-virtualized";
 
 class App extends Component {
-  renderRow = ({ index, isScrolling, key, style }) => {
+  renderRow = ({ index, isScrolling, key, style}) => {
     return (
       <div key={key} style={style}>
         <div>{this.props.data[index].name}</div>
         <div>{this.props.data[index].email}</div>
       </div>
-    );
-  };
+    )
+  }
   render() {
     return (
       <AutoSizer>
-        {({ width, height }) => {
-          return (
-            <List
-              rowCount={this.props.data.length}
-              width={width}
-              height={height}
-              rowHeight={50}
-              rowRenderer={this.renderRow}
-            />
-          );
-        }}
+      {
+        ({ width, height }) => {
+          return <List
+            rowCount={this.props.data.length}
+            width={width}
+            height={height}
+            rowHeight={50}
+            rowRenderer={this.renderRow}
+          />
+        }
+      }
       </AutoSizer>
     );
   }
